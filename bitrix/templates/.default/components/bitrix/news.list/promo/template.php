4ebbfae7b13b6e1356fa7c3adfeb9a48aabe01df
Andrey Chechkin <andrey.chechkin@web-izmerenie.ru>
2015-03-23 13:43:49 +0300

    hide block pormo from search bot

--- bitrix/templates/.default/components/bitrix/news.list/promo/template.php
+++ bitrix/templates/.default/components/bitrix/news.list/promo/template.php
@@ -13,42 +13,44 @@
 $this->setFrameMode(true);
 ?>        
 <?if(count($arResult["ITEMS"])>0):?>
-    <div class="promo-index">
-        <div class="row">
-        <?foreach($arResult["ITEMS"] as $arItem):?>   
-            <?
-            $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
-            $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
-            ?>
-            <?
-            $i++;
-            $arData = array(
-                'NAME'=>$arItem['NAME'],
-                'PREVIEW_TEXT'=>$arItem['PREVIEW_TEXT']
-            );
-            if($arItem['PREVIEW_PICTURE']['ID']){
-                $arData['PREVIEW_PICTURE'] = CFile::ResizeImageGet($arItem['PREVIEW_PICTURE']['ID'], array('width'=>100, 'height'=>100), BX_RESIZE_IMAGE_EXACT);
-            }
-            ?>
-            <div class="col-md-6">
-                <div class="item" style="background:url(<?=$arData['PREVIEW_PICTURE']['src']?>) no-repeat left top">
-                    <div class="name"><?=$arData['NAME']?></div>
-                    <div class="text"><?=$arData['PREVIEW_TEXT']?></div>
-                </div>                                 
-            </div>
-            <?
-            $bClear = false;
-            if($i%2 == 0){
-                $bClear = true;
-            }
-            if($i == count($arResult["ITEMS"])){
-                $bClear = true;
-            }
-            ?>
-            <?if($bClear){?>
-                <div class="clearfix"></div>
-            <?}?>
-        <?endforeach;?>   
-        </div>
-    </div>  
+	<!--noindex-->
+		<div class="promo-index">
+			<div class="row">
+			<?foreach($arResult["ITEMS"] as $arItem):?>   
+				<?
+				$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+				$this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+				?>
+				<?
+				$i++;
+				$arData = array(
+					'NAME'=>$arItem['NAME'],
+					'PREVIEW_TEXT'=>$arItem['PREVIEW_TEXT']
+				);
+				if($arItem['PREVIEW_PICTURE']['ID']){
+					$arData['PREVIEW_PICTURE'] = CFile::ResizeImageGet($arItem['PREVIEW_PICTURE']['ID'], array('width'=>100, 'height'=>100), BX_RESIZE_IMAGE_EXACT);
+				}
+				?>
+				<div class="col-md-6">
+					<div class="item" style="background:url(<?=$arData['PREVIEW_PICTURE']['src']?>) no-repeat left top">
+						<div class="name"><?=$arData['NAME']?></div>
+						<div class="text"><?=$arData['PREVIEW_TEXT']?></div>
+					</div>                                 
+				</div>
+				<?
+				$bClear = false;
+				if($i%2 == 0){
+					$bClear = true;
+				}
+				if($i == count($arResult["ITEMS"])){
+					$bClear = true;
+				}
+				?>
+				<?if($bClear){?>
+					<div class="clearfix"></div>
+				<?}?>
+			<?endforeach;?>   
+			</div>
+		</div> 
+	<!--/noindex-->
 <?endif?>
